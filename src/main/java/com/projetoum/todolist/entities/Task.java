3c211b1c5ae08f54d3f4b6bcc99b3bf14e888f41
Lorenzo Bonatto <lorenzobonattop@gmail.com>
2025-05-23 10:42:07 -0300

    Entities: Task and TaskStatus

---
+++ src/main/java/com/projetoum/todolist/entities/Task.java
@@ -0,0 +1,41 @@
+package com.projetoum.todolist.entities;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.*;
+
+import java.time.LocalDateTime;
+import java.util.Objects;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Task {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String title;
+    private String description;
+    private TaskStatus status;
+    private String createdBy;
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        Task task = (Task) o;
+        return Objects.equals(id, task.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(id);
+    }
+}
---
+++ src/main/java/com/projetoum/todolist/entities/TaskStatus.java
@@ -0,0 +1,7 @@
+package com.projetoum.todolist.entities;
+
+public enum TaskStatus {
+    COMPLETED,
+    PENDING,
+    CANCELLED
+}
